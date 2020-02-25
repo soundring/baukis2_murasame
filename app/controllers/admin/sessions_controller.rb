1acf7a5af4b450db79324b334501feef6f0c4208
soundring <soundringing@yahoo.co.jp>
2020-02-26 00:11:58 +0900

    chapter10

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -14,7 +14,7 @@ class Admin::SessionsController < Admin::Base
       administrator =
         Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
     end
-    if Admin::Authenticator.new(staff_member).authenticate(@form.password)
+    if Admin::Authenticator.new(administrator).authenticate(@form.password)
       if administrator.suspended?
         flash.now.alert = "アカウントが停止されています。"
         render action: "new"
